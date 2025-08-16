414f30151be61be8c46cc6d31f7dcf75cb628708
osanchez25 <osanchez25@hotmail.com>
2025-08-15 21:30:31 -0600

    Make back button return to index and enlarge title bars

--- back.js
+++ back.js
@@ -1,6 +1,8 @@
 (function () {
   const btn = document.querySelector('.back-arrow');
   if (btn) {
-    btn.addEventListener('click', () => window.history.back());
+    btn.addEventListener('click', () => {
+      window.location.href = 'index.html';
+    });
   }
 })();
